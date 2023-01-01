0f2a78cfb2a94ffbc68433382ca83930b0ac706e
micha <harasim1999@gmail.pl>
2023-01-01 17:43:29 +0100

    done frontend

--- frontend/src/components/Notes/Notes.js
+++ frontend/src/components/Notes/Notes.js
@@ -1,25 +1,69 @@
 import React from "react";
 import './Notes.css';
 import Note from './Note/Note';
+import NewNote from './NewNote/NewNote';
+import Modal from  'react-modal';
+import EditNote from "../EditNote/EditNote";
+
+
 class Notes extends React.Component {
 
     constructor(props){
         super(props);
 
-
-        this.notes =[
-            {
-                id: '2323',
-                title: 'Wykąpać psa',
-                body: 'pamietaj aby wykapac psa specjalnym szaponem'
-            },
-            {
-                id: '4232',
-                title: 'Zrobic zakupy',
-                body: 'kupic mleko, masło, likier'
-            }
-        ]
+        this.state ={
+            notes: [
+                {
+                    id: '2323',
+                    title: 'Wykąpać psa',
+                    body: 'pamietaj aby wykapac psa specjalnym szaponem'
+                },
+                {
+                    id: '4232',
+                    title: 'Zrobic zakupy',
+                    body: 'kupic mleko, masło, likier'
+                }
+            ],
+            showEditModal: false,
+            editNote: {}
+        };
     }
+    
+
+    deleteNote(id){
+        console.log('usuwanie notatki');
+        const notes = [...this.state.notes]
+                .filter(note => note.id !== id);
+        this.setState({notes}); 
+    }
+
+    addNote(note){
+        const notes = [...this.state.notes];
+        notes.push(note);
+        this.setState({notes});
+    }
+    
+    editNote(note){
+        const notes = [...this.state.notes];
+        const index = notes.findIndex(x => x.id === note.id);
+        if (index >=0){
+            notes[index] = note; 
+            this.setState({notes});
+        } 
+        this.toggleModal();
+    }
+
+  toggleModal(){
+        this.setState({showEditModal: !this.state.showEditModal
+        });    
+    }
+
+    editNoteHandler(note){
+        this.toggleModal();
+        this.setState({ editNote: note})
+    }
+
+    
     render() {
 
           
@@ -27,11 +71,33 @@ class Notes extends React.Component {
             <div>
                 <p>Moje notatki</p>
 
-                {this.notes.map(note =>(
+                <NewNote
+                onAdd={(note) => this.addNote(note)} />
+
+
+                <Modal 
+                isOpen={this.state.showEditModal}
+                contentLabel="Edytuj notatkę" >
+                
+                    <EditNote
+                    title = {this.state.editNote.title}
+                    body = {this.state.editNote.body}
+                    id = {this.state.editNote.id}
+
+                    onEdit={note => this.editNote(note)}/>
+                    <button onClick ={() => this.toggleModal()}>Anuluj</button>
+                    
+                </Modal>
+
+
+                {this.state.notes.map(note =>(
                     <Note 
+                    key ={note.id }
                     title= {note.title}
                     body= {note.body}
-                    id = {note.id} />
+                    id = {note.id} 
+                    onEdit = {(id) => this.editNoteHandler(note)}
+                    onDelete = {(id) => this.deleteNote(id)} />
                     ))}
             
              </div> 
